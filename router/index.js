6acde029fedfc2a57ba5443cdabe9f0e3c23c937
davidhong <taehwan.hong@gmail.com>
2017-03-28 12:28:57 +0900

    index.js update

--- router/index.js
+++ router/index.js
@@ -5,8 +5,8 @@ var router = express.Router(); //라우터 메소드 시행
 var path = require('path'); //경로 조정하기 위한 메소드
 
 var main = require('./main/main.js');
-var email = require('./email/email.js');
-var join = require('./join/index.js'); // .../join으로 들어오면 join/index.js라우팅
+// var email = require('./email/email.js');
+// var join = require('./join/index.js'); // .../join으로 들어오면 join/index.js라우팅
 
 //root routing
 router.get('/', function(req, res) {
@@ -15,8 +15,8 @@ router.get('/', function(req, res) {
 
 //route
 router.use('/main', main); //다른모듈을 쓸때는 use쓰면 됌
-router.use('/email', email);
-router.use('/join', join);
-router.use('/login', login);//login 연결
+// router.use('/email', email);
+// router.use('/join', join);
+// router.use('/login', login);//login 연결
 
 module.exports = router;
